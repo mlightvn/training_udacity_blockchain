999501fba3b590a26dceb2eda95e26ecafad4c06
Ngoc Nam <19430488+mlightvn@users.noreply.github.com>
2022-06-05 01:00:30 +0900

    env file

--- proj02/configuration.js
+++ proj02/configuration.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
     web3Provider: {
       // The endpoint that the server will listen on
@@ -5,13 +7,13 @@ module.exports = {
       // endpoint: 'ws://localhost:7545',
     },
     // The private key of the sender
-    privateKey: 'edba846ecfebf9abb8435071934b211f07e48685084ab458ec4fc1aa8f1245c0',
+    privateKey: process.env.SENDER_PRIVATE_KEY,
     // The address of the sender
-    sendingAddress: '0xBeE436d17D429BFdaD2491478f77AF656699EA1b',
+    sendingAddress: process.env.SENDER_ADDRESS,
     // The address of the receiver
-    receivingAddress: '0x551D492FEb84c548068A6002E69Cffd3D804Cbe6',
+    receivingAddress: process.env.RECEIVER_ADDRESS,
 
-    metamaskAddress: '0x27b5419afb9c3D99A262046383e083Ae7d81900a',
+    metamaskAddress: process.env.METAMASK_ADDRESS, // '0x27b5419afb9c3D99A262046383e083Ae7d81900a'
 
     nonceUInt: 1,
     weiToEtherUnit: 1000000000000000000, // 1,000,000,000,000,000,000 = 1 Ether
