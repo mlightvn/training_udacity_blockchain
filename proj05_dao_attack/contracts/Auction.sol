83d5bfbb16e36300bce84237ae8ca18c64c85b21
Ngoc Nam <19430488+mlightvn@users.noreply.github.com>
2022-07-05 22:53:56 +0900

    project 05

---
+++ proj05_dao_attack/contracts/Auction.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.16;
+
+// INSECURE
+contract Auction {
+    address currentLeader;
+    uint256 highestBid;
+
+    function bid() payable {
+        require(msg.value > highestBid);
+
+        require(currentLeader.send(highestBid)); // Refund the old leader, if it fails then revert
+
+        currentLeader = msg.sender;
+        highestBid = msg.value;
+    }
+}
